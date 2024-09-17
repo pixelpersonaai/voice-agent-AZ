4762c7b8547e0f779d1a1f7eca4d74d4df0b2899
Ziyan Liu <zliucareer@proton.me>
2024-09-17 00:44:32 -0500

    Added blurred background to chat history

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,19 @@ import { Message, useChat } from "ai/react";
 import {
   AudioLinesIcon,
   CircleUserIcon,
+  LogOutIcon,
   MicIcon,
   Sparkles,
   SquareIcon,
 } from "lucide-react";
 import { useState } from "react";
+import {
+  AudioConfig,
+  ResultReason,
+  SpeechConfig,
+  SpeechRecognizer,
+  SpeechSynthesizer,
+} from "microsoft-cognitiveservices-speech-sdk";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
@@ -49,96 +57,78 @@ export default function Home() {
       <div className="min-h-screen bg-white w-full bg-slate-100">
         {/*  */}
         <div className="w-full flex flex-col items-center justify-center h-[98vh] overflow-y-auto pb-2">
-          <div className="w-3/5 flex flex-col justify-center bg-white rounded-lg p-4 shadow-md">
-            {messages.map((message: Message, index: number) => {
-              return (
-                // Chat History
-                <div
-                  key={message.id}
-                  className="flex flex-col gird grid-cols-8"
-                >
-                  {message.content
-                    .split("\n")
-                    .map((currentText: string, index: number) => {
-                      if (currentText === "") {
-                        return <p key={message.id}>&nbsp;&nbsp;&nbsp;</p>;
-                      } else {
-                        return (
-                          <>
-                            {message.role === "user" ? (
-                              <div className="flex my-2 justify-end">
-                                <div
-                                  key={message.id + index}
-                                  className="bg-sky-100 text-sm text-blue-700 rounded-lg px-2 py-1"
-                                >
-                                  {currentText}
+          <div className="relative w-3/5 flex flex-col justify-center bg-white rounded-lg shadow-md">
+            <div className="absolute inset-0 m-0.5 bg-gradient-to-br from-pink-300 via-yellow-300 via-blue-500 to-purple-600 rounded-lg blur"></div>
+            <div className="relative flex flex-col justify-center rounded-lg bg-white">
+              {messages.map((message: Message, index: number) => {
+                return (
+                  // Chat History
+                  <div
+                    key={message.id}
+                    className="flex flex-col gird grid-cols-8"
+                  >
+                    {message.content
+                      .split("\n")
+                      .map((currentText: string, index: number) => {
+                        if (currentText === "") {
+                          return <p key={message.id}>&nbsp;&nbsp;&nbsp;</p>;
+                        } else {
+                          return (
+                            <>
+                              {message.role === "user" ? (
+                                <div className="flex my-2 justify-end">
+                                  <div
+                                    key={message.id + index}
+                                    className="bg-sky-100 text-sm text-blue-700 rounded-lg px-2 py-1"
+                                  >
+                                    {currentText}
+                                  </div>
+                                  <div className="mx-2">
+                                    <button className="rounded-full bg-blue-500 p-1 text-white">
+                                      <CircleUserIcon size={24} />
+                                    </button>
+                                  </div>
                                 </div>
-                                <div className="mx-2">
-                                  <button className="rounded-full bg-blue-500 p-1 text-white">
-                                    <CircleUserIcon size={24} />
-                                  </button>
+                              ) : (
+                                <div className="flex my-2 justify-start">
+                                  <div className="mx-2">
+                                    <button className="rounded-full bg-sky-100 p-1 text-white">
+                                      <Sparkles
+                                        className="text-blue-600 p-0.5"
+                                        strokeWidth={2}
+                                        size={22}
+                                      />
+                                    </button>
+                                  </div>
+                                  <div
+                                    key={message.id + index}
+                                    className="bg-blue-500 text-sm text-white px-2 py-1 rounded-lg"
+                                  >
+                                    {currentText}
+                                  </div>
                                 </div>
-                              </div>
-                            ) : (
-                              <div className="flex my-2 justify-start">
-                                <div className="mx-2">
-                                  <button className="rounded-full bg-sky-100 p-2 text-white">
-                                    <Sparkles
-                                      className="text-blue-600"
-                                      size={20}
-                                    />
-                                  </button>
-                                </div>
-                                <div
-                                  key={message.id + index}
-                                  className="bg-blue-500 text-sm text-white px-2 py-1 rounded-lg"
-                                >
-                                  {currentText}
-                                </div>
-                              </div>
-                            )}
-                          </>
-                        );
-                      }
-                    })}
-                </div>
-              );
-            })}
+                              )}
+                            </>
+                          );
+                        }
+                      })}
+                  </div>
+                );
+              })}
+            </div>
           </div>
           {/* Input */}
           <div className="h-fit w-[415px] flex flex-col justify-between items-end my-2">
-            <div className="flex space-x-4">
-              {recordingReady ? (
-                <div className="flex items-center bg-blue-500 text-white rounded-lg px-4 font-bold w-[352px] h-[48px] text-blue-600 bg-white ">
-                  <span className="flex justify-center items-center w-full text-gray-400">
-                    <AudioLinesIcon size={24} />
-                    <AudioLinesIcon size={24} />
-                    <AudioLinesIcon size={24} />
-                    <AudioLinesIcon size={24} />
-                  </span>
-                  <button
-                    className="flex justify-end items-center p-0.5 border border-transparent  hover:border-red-500 hover:rounded-sm"
-                    onClick={() => {
-                      setRecordingReady((prev) => !prev);
-                    }}
-                  >
-                    <SquareIcon
-                      size={20}
-                      className="bg-red-500 text-red-500 rounded-sm"
-                    />
-                  </button>
-                </div>
-              ) : (
-                <button
-                  className="bg-blue-500 bottom-0 inset-x-0 rounded-lg px-4 font-bold w-[352px] h-[48px] text-blue-500 bg-white hover:bg-blue-500 hover:text-white"
-                  onClick={() => {
-                    setRecordingReady((prev) => !prev);
-                    setRecordingStared((prev) => !prev);
-                  }}
-                >
-                  Speak to Agent
-                </button>
-              )}
+            <div className="w-full flex justify-center items-center">
+              <button
+                className="flex justify-center text-sm items-center bg-white bottom-0 inset-x-0 rounded-lg px-4 font-semibold font-sans  h-[48px] text-blue-500 hover:bg-blue-500 hover:text-white"
+                onClick={() => {
+                  setRecordingReady((prev) => !prev);
+                  setRecordingStared((prev) => !prev);
+                }}
+              >
+                <LogOutIcon size={16} className="mr-2" /> End Interview
+              </button>
             </div>
           </div>
         </div>
